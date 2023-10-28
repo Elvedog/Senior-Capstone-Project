b780215b171c8e7ab5c47f3a250d951f54e21866
Elvedog <116691070+Elvedog@users.noreply.github.com>
2023-10-27 21:26:30 -0500

    Update employee.java

--- employee.java
+++ employee.java
@@ -1,30 +1,70 @@
 package employedatabasesystem;
 
 public class Employee {
-	
-	    private String name;
-	    private String id;
+    private String name;
+    private String id;
+    private String address;
+    private String phoneNumber;
+    private int timeWorked;
 
-	    public Employee(String name, String id) {
-	        this.name = name;
-	        this.id = id;
-	    }
+    public Employee(String name, String id, String address, String phoneNumber, int timeWorked) {
+        this.name = name;
+        this.id = id;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.timeWorked = timeWorked;
+    }
 
-	    public String getName() {
-	        return name;
-	    }
+    // Getters and setters for all fields
 
-	    public String getId() {
-	        return id;
-	    }
+    public String getName() {
+        return name;
+    }
 
-	    public void setName(String name) {
-	        this.name = name;
-	    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	    @Override
-	    public String toString() {
-	        return "Name: " + name + ", ID: " + id;
-	    }
-	}
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getTimeWorked() {
+        return timeWorked;
+    }
+
+    public void setTimeWorked(int timeWorked) {
+        this.timeWorked = timeWorked;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", timeWorked=" + timeWorked +
+                '}';
+    }
+}
