25b2cf0b0bec17d940c9a842e33ca143f7804f5e
Elvedog <116691070+Elvedog@users.noreply.github.com>
2023-11-24 02:55:29 -0600

    Update employee.java

--- employee.java
+++ employee.java
@@ -6,17 +6,24 @@ public class Employee {
     private String address;
     private String phoneNumber;
     private int timeWorked;
-
+    private String[] workSchedule;
     public Employee(String name, String id, String address, String phoneNumber, int timeWorked) {
         this.name = name;
         this.id = id;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.timeWorked = timeWorked;
+        this.workSchedule = new String[7];
     }
 
     // Getters and setters for all fields
+    public String[] getWorkSchedule() {
+        return workSchedule;
+    }
 
+    public void setWorkSchedule(String[] workSchedule) {
+        this.workSchedule = workSchedule;
+    }
     public String getName() {
         return name;
     }
@@ -53,6 +60,12 @@ public class Employee {
         return timeWorked;
     }
 
+    public String getWorkScheduleForDay(int dayIndex) {
+        if (dayIndex >= 0 && dayIndex < workSchedule.length) {
+            return workSchedule[dayIndex];
+        }
+        return ""; // Return an empty string or another appropriate value if out of bounds.
+    }
     public void setTimeWorked(int timeWorked) {
         this.timeWorked = timeWorked;
     }
@@ -67,4 +80,31 @@ public class Employee {
                 ", timeWorked=" + timeWorked +
                 '}';
     }
+
+	public void setScheduledTime(int newScheduledTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setTasks(String newTasks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSalary(double newSalary) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getTasks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	public void setWorkScheduleForDay(int i, String newWorkSchedule) {
+		// TODO Auto-generated method stub
+		
+	}
 }
